a61a5e2cdd5debc2232cb6b90d63c196a2fc19ae
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2023-09-15 22:06:53 +0100

    fix: Add project details edit form

--- src/lib/project/ProjectHTMLWrapper.js
+++ src/lib/project/ProjectHTMLWrapper.js
@@ -1,24 +1,34 @@
 "use strict";
 
+import { v4 as uuidv4 } from "uuid";
+
 export { ProjectHTMLWrapper as default };
 
 class ProjectHTMLWrapper {
   static generateProjectHTMLElement(project, deleteFunction = null) {
     const element = document.createElement("div");
+    element.dataset.uuid = uuidv4();
 
     const header = element.appendChild(document.createElement("header"));
 
     const headerTextContainer = header.appendChild(
       document.createElement("div"),
     );
+    headerTextContainer.classList.add("header-text-container");
 
     const title = headerTextContainer.appendChild(document.createElement("h1"));
     title.textContent = project.title;
+    title.dataset.formLabel = "Title";
 
     const description = headerTextContainer.appendChild(
       document.createElement("h2"),
     );
     description.textContent = project.description;
+    description.dataset.formLabel = "Description";
+
+    const headerTextEditForm = header.appendChild(
+      this._generateFormElement(element),
+    );
 
     const headerBtnContainer = header.appendChild(
       document.createElement("div"),
@@ -31,6 +41,9 @@ class ProjectHTMLWrapper {
     editBtn.type = "button";
     editBtn.innerHTML =
       "<span class='material-symbols-outlined'>edit_square</span>";
+    editBtn.addEventListener("click", () => {
+      header.classList.toggle("editing");
+    });
 
     const deleteBtn = headerBtnContainer.appendChild(
       document.createElement("button"),
@@ -51,5 +64,42 @@ class ProjectHTMLWrapper {
     );
 
     return element;
+  }
+
+  static _generateFormElement(projectElement) {
+    const formElement = document.createElement("form");
+    formElement.classList.add("header-text-form");
+
+    for (const editableElement of projectElement.querySelectorAll(
+      "[data-form-label]",
+    )) {
+      const label = formElement.appendChild(document.createElement("label"));
+      label.textContent = editableElement.dataset.formLabel;
+
+      const input = formElement.appendChild(document.createElement("input"));
+      input.id =
+        projectElement.dataset.uuid + "-" + label.textContent.toLowerCase();
+      label.htmlFor = input.id;
+      input.value = editableElement.textContent;
+    }
+
+    const buttonContainer = formElement.appendChild(
+      document.createElement("div"),
+    );
+    buttonContainer.classList.add("form-btn-container");
+
+    const saveBtn = buttonContainer.appendChild(
+      document.createElement("button"),
+    );
+    saveBtn.type = "button";
+    saveBtn.textContent = "Save";
+
+    const cancelBtn = buttonContainer.appendChild(
+      document.createElement("button"),
+    );
+    cancelBtn.type = "button";
+    cancelBtn.textContent = "Cancel";
+
+    return formElement;
   }
 }
