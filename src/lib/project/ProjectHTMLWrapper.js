c511e25d2c4e8a8981a80f50142ec44b19dd52d7
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2023-09-15 19:41:33 +0100

    fix: Add project header

--- src/lib/project/ProjectHTMLWrapper.js
+++ src/lib/project/ProjectHTMLWrapper.js
@@ -4,10 +4,20 @@ export { ProjectHTMLWrapper as default };
 
 class ProjectHTMLWrapper {
   static generateProjectHTMLElement(project) {
-    // TODO: implement method
-
     const element = document.createElement("div");
-    element.textContent = project.title;
+
+    const header = element.appendChild(document.createElement("header"));
+
+    const title = header.appendChild(document.createElement("h1"));
+    title.textContent = project.title;
+
+    const description = header.appendChild(document.createElement("h2"));
+    description.textContent = project.description;
+
+    const todoContainer = element.appendChild(
+      document.createElement("todo-container"),
+    );
+
     return element;
   }
 }
