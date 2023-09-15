84a3caf2e330ae06c89ff6591755dd15478582cc
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2023-09-15 19:54:50 +0100

    fix: Add project buttons

--- src/lib/project/ProjectHTMLWrapper.js
+++ src/lib/project/ProjectHTMLWrapper.js
@@ -8,11 +8,36 @@ class ProjectHTMLWrapper {
 
     const header = element.appendChild(document.createElement("header"));
 
-    const title = header.appendChild(document.createElement("h1"));
+    const headerTextContainer = header.appendChild(
+      document.createElement("div"),
+    );
+
+    const title = headerTextContainer.appendChild(document.createElement("h1"));
     title.textContent = project.title;
 
-    const description = header.appendChild(document.createElement("h2"));
+    const description = headerTextContainer.appendChild(
+      document.createElement("h2"),
+    );
     description.textContent = project.description;
+
+    const headerBtnContainer = header.appendChild(
+      document.createElement("div"),
+    );
+    headerBtnContainer.classList.add("header-btn-container");
+
+    const editBtn = headerBtnContainer.appendChild(
+      document.createElement("button"),
+    );
+    editBtn.type = "button";
+    editBtn.innerHTML =
+      "<span class='material-symbols-outlined'>edit_square</span>";
+
+    const deleteBtn = headerBtnContainer.appendChild(
+      document.createElement("button"),
+    );
+    deleteBtn.type = "button";
+    deleteBtn.innerHTML =
+      "<span class='material-symbols-outlined'>delete</span>";
 
     const todoContainer = element.appendChild(
       document.createElement("todo-container"),
