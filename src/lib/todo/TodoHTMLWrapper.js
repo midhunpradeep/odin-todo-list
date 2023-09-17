62e9f412431721e635e6bec8df61bfd1765a2c99
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2023-09-17 21:01:15 +0100

    fix: Add button to expand todos

--- src/lib/todo/TodoHTMLWrapper.js
+++ src/lib/todo/TodoHTMLWrapper.js
@@ -29,11 +29,45 @@ class TodoHTMLWrapper {
     // TODO: implement html
     const title = this.htmlElement.appendChild(document.createElement("h4"));
     title.textContent = this.todo.title;
+    title.classList.add("always-visible");
 
     const dueDate = this.htmlElement.appendChild(document.createElement("p"));
     dueDate.textContent = "Finish before: " + this.todo.dueDate.toDateString();
     dueDate.classList.add("todo-due-date");
+    dueDate.classList.add("always-visible");
 
     this.htmlElement.dataset.priority = this.todo.priority;
+
+    const description = this.htmlElement.appendChild(
+      document.createElement("p"),
+    );
+    description.textContent = this.todo.description;
+
+    const expandBtn = this.htmlElement.appendChild(
+      document.createElement("button"),
+    );
+    expandBtn.type = "button";
+    expandBtn.classList.add("always-visible");
+    expandBtn.classList.add("todo-expand-btn");
+
+    expandBtn.updateText = () => {
+      expandBtn.innerHTML = this.htmlElement.classList.contains("expanded")
+        ? "<span class='material-symbols-outlined'>expand_less</span>"
+        : "<span class='material-symbols-outlined'>expand_more</span>";
+    };
+    expandBtn.updateText();
+
+    expandBtn.addEventListener("click", () => {
+      if (!this.htmlElement.classList.contains("expanded")) {
+        const otherExpandedElements =
+          this.htmlElement.parentElement.querySelectorAll(".expanded");
+        for (const expandedElement of otherExpandedElements) {
+          expandedElement.classList.remove("expanded");
+        }
+      }
+
+      this.htmlElement.classList.toggle("expanded");
+      expandBtn.updateText();
+    });
   }
 }
